98318565872867e738ddc2247b3846a9bc838cda
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-02-10 02:30:55 +0530

    database fixed

--- fsdb.sql
+++ fsdb.sql
@@ -5,16 +5,17 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `details`;
 CREATE TABLE `details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `college_name` varchar(255) NOT NULL,
   `team_name` varchar(255) NOT NULL,
   `team_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `team_name` (`team_name`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `participant`;
 CREATE TABLE `participant` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -22,15 +23,12 @@ CREATE TABLE `participant` (
   `phone` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `team_id` varchar(255) NOT NULL,
-  `pid` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `usn` (`usn`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `phone` (`phone`),
-  UNIQUE KEY `pid` (`pid`)
+  `pid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -50,4 +48,4 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (8,	'user7',	'fashion@incridea'),
 (9,	'user8',	'delta@incridea');
 
--- 2018-02-09 18:40:49
+-- 2018-02-09 20:59:08
